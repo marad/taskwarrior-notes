7601d1fc128778e48a4530b882ad3899562ffc54
Marcin Radoszewski <marcin.radoszewski@allegro.com>
2025-07-01 12:23:38 +0200

    Remove unused toggle flag from root command initialization

--- cmd/root.go
+++ cmd/root.go
@@ -27,8 +27,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "taskwarrior-notes",
@@ -62,7 +60,4 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
